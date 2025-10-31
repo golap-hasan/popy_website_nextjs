0fcb8e03320a78b12730e664b70689d8e34fb157
nurulla-hasan <nurullahasan.dev@gmail.com>
2025-10-31 14:58:41 +0600

    space added

--- src/app/(main-route)/not-found.tsx
+++ src/app/(main-route)/not-found.tsx
@@ -13,7 +13,7 @@ export default function NotFound() {
             <div className="mx-auto mb-4 grid size-16 place-items-center rounded-full bg-primary/10 text-primary">
               <Ghost className="size-8" />
             </div>
-            <h1 className="text-4xl md:text-5xl font-semibold tracking-tight">
+            <h1 className="text-4xl md:text-5xl font-semibold tracking-tight"> 
               404
               <span className="sr-only"> - </span>
               <span className="bg-linear-to-r from-primary to-primary/60 bg-clip-text text-transparent ml-2">
