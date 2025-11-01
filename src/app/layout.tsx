4408daa9a2caaea7fea55a09c1dd988ebcf95940
nurulla-hasan <nurullahasan.dev@gmail.com>
2025-11-01 14:32:32 +0600

    update layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme/theme-provider";
-import { ScrollArea } from "@/components/ui/scroll-area";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -35,9 +34,7 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
-          <ScrollArea className="screen-height">
             {children}
-          </ScrollArea>
         </ThemeProvider>
       </body>
     </html>
