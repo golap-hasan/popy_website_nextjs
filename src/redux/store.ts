26ad537d0e59d03a326dfd64f2f5054841a17dcd
nurulla-hasan <nurullahasan.dev@gmail.com>
2025-11-17 23:36:59 +0600

    update store

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,5 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { persistStore, persistReducer } from "redux-persist";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
 import storage from "redux-persist/lib/storage";
 import cartReducer from "./feature/cart/cartSlice";
 import wishlistReducer from "./feature/wishlist/wishListSlice";
@@ -25,7 +34,7 @@ export const store = configureStore({
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
       serializableCheck: {
-        ignoredActions: ["persist/PERSIST", "persist/REHYDRATE"],
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
       },
     }),
 });
